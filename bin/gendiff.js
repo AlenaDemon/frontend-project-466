5b74881e8daa833ebbcb9ca5b0dd99022142b84f
AlenaDemon <drmkpr@mail.ru>
2025-03-04 14:05:42 +0400

    arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,9 @@ program
 
 program
   .description('Compares two configuration files and shows a difference.')
-
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
 
